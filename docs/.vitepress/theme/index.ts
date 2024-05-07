90721f69e9c83cd03d637c42d7cd2b82c66bf410
xingjunyang <113870551+xing-junyang@users.noreply.github.com>
2024-05-07 22:12:19 +0800

    增加了文章脚标

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,11 +3,20 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import './components/MyComponent.vue'
+
+const MyComponent = {
+  setup() {
+    return () => h('div',{class:'footer'}, [h('p', 'This work is licensed under CC BY 4.0'),h('p','XINGJUNYANG © 2024')])
+  }
+}
 
 export default {
   extends: DefaultTheme,
-  // Layout: () => h('div', 'Hello, VitePress!'),
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-footer-before': () => h(MyComponent)
+    })
+  },
   enhanceApp({ app, router, siteData }) {
     // ...
   },
