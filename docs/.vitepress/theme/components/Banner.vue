bc33adb2e1f0a02a8d12437d4a5a326fba90fa67
xingjunyang <xingjunyang@smail.nju.edu.cn>
2025-01-04 13:46:14 +0800

    feat: banner

--- docs/.vitepress/theme/components/Banner.vue
+++ docs/.vitepress/theme/components/Banner.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 const showBanner = ref(sessionStorage.getItem('close_banner') !== 'true');
 
@@ -12,10 +12,21 @@ function close() {
 <template>
 	<div class="banner" v-if="showBanner">
 		<div class="banner-content">
-			<span class="banner-title">期末周顺利🙏</span>
+			<div class="banner-content-container">
+			<span class="banner-title">
+				✨机器学习复习重点已出炉✨
+			</span>
+				<a class="link"
+				   href="https://xjynotes.top/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0%E5%A4%8D%E4%B9%A0%E9%87%8D%E7%82%B9.html">Check
+					it out!</a>
+			</div>
 		</div>
 		<div class="close" @click="close">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+				<!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+				<path
+					d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+			</svg>
 		</div>
 	</div>
 </template>
@@ -39,12 +50,31 @@ function close() {
 	padding: 0 20px;
 }
 
+.banner-content-container {
+	display: flex;
+	align-items: center;
+	flex-direction: row;
+	gap: 10px;
+}
+
 .banner-title {
 	font-size: 16px;
 	font-weight: bold;
 }
 
-.close{
+.link {
+	text-decoration: none;
+	border-radius: 16px;
+	padding: 0px 12px;
+	margin: 0 4px;
+	background-color: #bbdaff;
+	transition: background-color 0.3s, color 0.25s, opacity 0.25s;
+	font-size: 14px;
+	color: var(--vp-c-brand-1);
+	font-weight: bold;
+}
+
+.close {
 	position: absolute;
 	right: 20px;
 	top: 50%;
